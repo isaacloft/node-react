854f5dbc8c636b52f508e6c1d7ca9af345464415
isaac <isaac.guan@rxpservices.com>
2019-03-03 23:28:04 +1100

    add google oauth

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+    },
+    accessToken => {
+      console.log(accessToken);
+    },
+  ),
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] }),
+);
+
+app.get('/auth/google/callback', function (req, res) {
+  res.send('call back from oauth');
 });
 
 const PORT = process.env.PORT || 5000;
