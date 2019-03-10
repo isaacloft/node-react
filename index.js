3216fa1d5522286d80a9a8ec1bd114ae7a27149a
isaac <isaac.guan@rxpservices.com>
2019-03-10 21:15:47 +1100

    configured production keys and auth files

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
-const app = express();
-require('./routes/authRoutes')(app);
-
-// The order of below two require statements matters, as passport is using
-// User as model
 require('./models/User');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
+
+const app = express();
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey],
+    }),
+);
+app.use(passport.initialize());
+app.use(passport.session());
+require('./routes/authRoutes')(app);
+
+// The order of below two require statements matters, as passport is using
+// User as model
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,5 +6,15 @@ module.exports = app => {
     passport.authenticate('google', { scope: ['profile', 'email'] }),
   );
 
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
+
   app.get('/auth/google/callback', passport.authenticate('google'));
+
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+    // res.send(req.session);
+  });
 };
